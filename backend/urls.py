26acc66c746198823616a1fba80fc5ae89904de6
Ricardo D. Quiroga <l2radamanthys@gmail.com>
2022-03-29 00:12:21 -0300

    fix

--- backend/urls.py
+++ backend/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path
 import backend.settings as settings
 from django.views.generic.base import TemplateView, RedirectView
+from django.conf.urls.static import static
+from django.conf.urls import url, include
 
 
 urlpatterns = [
