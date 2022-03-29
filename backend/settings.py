fa12c3506751f09fdc6d326f0d525a6d6c2978d1
Ricardo D. Quiroga <l2radamanthys@gmail.com>
2022-03-29 00:04:43 -0300

    agregado static files

--- backend/settings.py
+++ backend/settings.py
@@ -10,6 +10,7 @@ env = environ.Env(
     DEBUG=(bool, True),
     DATABASE_URL=(str, ""),
     ALLOWED_HOSTS=(str, "ALLOWED_HOSTS"),
+    MEDIA_ROOT=(str, "media_archivos_locales"),
     CI=(bool, False),
 )
 environ.Env.read_env()
@@ -113,12 +114,13 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.0/howto/static-files/
+STATIC_ROOT = "staticfiles"
+STATIC_URL = "/static/"
 
-STATIC_URL = "static/"
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+    #os.path.join(BASE_DIR, "media_archivos_locales"),
+)
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+MEDIA_ROOT = os.environ.get("MEDIA_ROOT", "media_archivos_locales")
+MEDIA_URL = "/media/"
